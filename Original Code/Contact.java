68fa89f9ba9a5a760d422dafeaa586160a48b632
anthonynguyen504 <76272164+anthonynguyen504@users.noreply.github.com>
2024-10-13 08:58:08 -0700

    Update Contact.java

--- Original Code/Contact.java
+++ Original Code/Contact.java
@@ -1,65 +1,141 @@
 package contact;
 
+/**
+ * This class represents a Contact with attributes such as contact ID, 
+ * first name, last name, phone number, and address.
+ */
 public class Contact {
-   String contactID;
-   String firstName;
-   String lastName;
-   String phoneNumber;
-   String address;
-  
-   public Contact(String contactID, String firstName, String lastName, String phoneNumber, String address) {
-       super();
-       this.contactID = contactID;
-       this.firstName = firstName;
-       this.lastName = lastName;
-       this.phoneNumber = phoneNumber;
-       this.address = address;
-   }
-  
-   public String getContactID() {
-       return contactID;
-   }
+    // Attributes of the Contact class
+    private String contactID;    // Unique ID for the contact
+    private String firstName;    // First name of the contact
+    private String lastName;     // Last name of the contact
+    private String phoneNumber;  // Phone number of the contact
+    private String address;      // Address of the contact
 
-   public void setContactID(String contactID) {
-       this.contactID = contactID;
-   }
+    /**
+     * Constructor for the Contact class to initialize all fields.
+     * 
+     * @param contactID   The unique ID for the contact
+     * @param firstName   The first name of the contact
+     * @param lastName    The last name of the contact
+     * @param phoneNumber The contact's phone number (should be 10 digits)
+     * @param address     The contact's address
+     */
+    public Contact(String contactID, String firstName, String lastName, String phoneNumber, String address) {
+        this.contactID = contactID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
 
-   public String getFirstName() {
-       return firstName;
-   }
+    // Getter and Setter methods for each attribute
 
-   public void setFirstName(String firstName) {
-       this.firstName = firstName;
-   }
+    /**
+     * Gets the contact ID.
+     * 
+     * @return The contact's ID
+     */
+    public String getContactID() {
+        return contactID;
+    }
 
-   public String getLastName() {
-       return lastName;
-   }
+    /**
+     * Sets the contact ID.
+     * 
+     * @param contactID The unique ID to be set for the contact
+     */
+    public void setContactID(String contactID) {
+        this.contactID = contactID;
+    }
 
-   public void setLastName(String lastName) {
-       this.lastName = lastName;
-   }
+    /**
+     * Gets the first name of the contact.
+     * 
+     * @return The contact's first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public String getPhoneNumber() {
-       return phoneNumber;
-   }
+    /**
+     * Sets the first name of the contact.
+     * 
+     * @param firstName The first name to be set for the contact
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public void setPhoneNumber(String phoneNumber) {
-       this.phoneNumber = phoneNumber;
-   }
+    /**
+     * Gets the last name of the contact.
+     * 
+     * @return The contact's last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
 
-   public String getAddress() {
-       return address;
-   }
+    /**
+     * Sets the last name of the contact.
+     * 
+     * @param lastName The last name to be set for the contact
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public void setAddress(String address) {
-       this.address = address;
-   }
+    /**
+     * Gets the contact's phone number.
+     * 
+     * @return The contact's phone number
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-   @Override
-   public String toString() {
-       return "Contact [contactID=" + contactID + ", firstName=" + firstName + ", lastName=" + lastName
-               + ", phoneNumber=" + phoneNumber + ", address=" + address + "]";
-   }
-  
+    /**
+     * Sets the contact's phone number. Basic validation is done to check 
+     * if the phone number is a valid 10-digit number.
+     * 
+     * @param phoneNumber The phone number to be set for the contact
+     * @throws IllegalArgumentException if the phone number is not 10 digits long
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        // Basic validation for phone number format (can be expanded)
+        if (phoneNumber.matches("\\d{10}")) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new IllegalArgumentException("Invalid phone number. It should be 10 digits.");
+        }
+    }
+
+    /**
+     * Gets the contact's address.
+     * 
+     * @return The contact's address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Sets the contact's address.
+     * 
+     * @param address The address to be set for the contact
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * Returns a string representation of the contact, including all attributes.
+     * 
+     * @return A formatted string with the contact's details
+     */
+    @Override
+    public String toString() {
+        return "Contact [contactID=" + contactID + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", phoneNumber=" + phoneNumber + ", address=" + address + "]";
+    }
 }
